c126166c397f57be414e20be44c147390c83c728
salubaba10 <salman_ganatra@yahoo.com>
2023-06-27 19:23:02 +0100

    Fixed app routes,

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import { Banner } from "./components/Banner";
+import { BrowserRouter } from "react-router-dom";
+import Box from "@mui/material/Box";
 
+import { Banner } from "./components/Banner";
+import { AppRoutes } from "./AppRoutes";
+import { NavBar } from "./components/NavBar";
 export const App = () => {
   return (
-    <div className="App">
-      <Banner title="Recipies App" subTitle="Burn your calories" />
-    </div>
+    <BrowserRouter>
+      <Box>
+        <NavBar />
+        <Banner title="Recipies App" subTitle="Burn your calories" />
+        <AppRoutes />
+      </Box>
+    </BrowserRouter>
   );
 };
