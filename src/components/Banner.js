e575d898baf1a5cb05d4af6f41ce2255378ee7c7
nobdey92 <jordan_hobdey@hotmail.co.uk>
2023-06-28 20:02:13 +0100

    Basic banner componets added

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,30 @@
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Paper from "@mui/material/Paper";
+import { useNavigate } from "react-router-dom";
+import * as React from 'react';
+
 export const Banner = () => {
+  const navigate = useNavigate();
+  
   return (
-    <div>
-      Banner
-    </div>
+    <Paper elevation={3}>
+      <Stack
+        alignContent="center"
+        alignItems="center"
+        spacing={2}
+        sx={{ p: 2 }}
+      >
+        <Typography component="h1" variant="h5">
+          Title
+        </Typography>
+        <Typography component="h2" variant="caption">
+         Sub-Title
+        </Typography>
+        <Typography>
+          Review-Text
+        </Typography>                
+      </Stack>
+    </Paper>
   );
 };
