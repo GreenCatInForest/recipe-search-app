4f70df31e76a47601e0f8f11479d8c31cbdc61f1
nobdey92 <jordan_hobdey@hotmail.co.uk>
2023-06-28 20:18:26 +0100

    recipie Cards and container added to explore page

--- src/components/RecipesContainer.js
+++ src/components/RecipesContainer.js
@@ -1,7 +1,10 @@
+import { RecipeCard } from "../components/RecipeCard"
+
 export const RecipesContainer = () => {
     return (
       <div>
         RecipesContainer
+        <RecipeCard />
       </div>
     );
   };
--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,9 +1,12 @@
 import { Footer } from "../components/Footer"
+import { SearchForm } from "../components/SearchForm"
+import { RecipesContainer } from "../components/RecipesContainer"
 
 export const Explore = () => {
     return (
       <div>
-        Explore
+        <SearchForm />
+        <RecipesContainer />
         <Footer />
       </div>
       
