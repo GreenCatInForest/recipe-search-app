018519eafe1e3548aed0c3c94f0b5dbd63dd6b09
GreenCatInForest <event2016.2017@gmail.com>
2023-06-29 19:50:01 +0100

    add FormControlLabel

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,64 +1,84 @@
+import * as React from "react";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { Stack } from "@mui/material";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import TextField from "@mui/material/TextField";
+import InputAdornment from "@mui/material/InputAdornment";
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
 
-import * as React from 'react';
-import { Formik } from 'formik';
-import Input from '@mui/base/Input';
-import { styled } from '@mui/system';
+export const SearchForm = ({ handleSubmit }) => {
+  const onSubmit = ({ searchTerm }) => {
+    handleSubmit(searchTerm);
+  };
 
-const CustomInput = React.forwardRef(function CustomInput(props, ref) {
-  return <Input slots={{ input: StyledInputElement }} {...props} ref={ref} />;
-});
+  const formik = useFormik({
+    initialValues: { searchTerm: "" },
+    onSubmit,
+    validationSchema: Yup.object({
+      searchTerm: Yup.string().required("Please enter a product name"),
+    }),
+  });
 
-export const SearchForm = () => {
-  return <CustomInput aria-label="input" placeholder="Type ingredient here..." />;
-}
+  return (
+    <Stack component="form" onSubmit={formik.handleSubmit} spacing={2}>
+      <TextField
+        error={formik.touched.searchTerm && !!formik.errors.searchTerm}
+        helperText={formik.errors.searchTerm}
+        name="searchTerm"
+        type="search"
+        label="Enter search term"
+        value={formik.values.searchTerm}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+        fullWidth
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton type="submit">
+                <SearchIcon />
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          //value={age}
+          label="Age"
+          //onChange={handleChange}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
 
-const blue = {
-  100: '#DAECFF',
-  200: '#80BFFF',
-  400: '#3399FF',
-  500: '#007FFF',
-  600: '#0072E5',
+      <FormGroup rowSpacing={3} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+        <FormControlLabel control={<Checkbox />} label="Label" />
+      </FormGroup>
+    </Stack>
+  );
 };
-
-const grey = {
-  50: '#F3F6F9',
-  100: '#E7EBF0',
-  200: '#E0E3E7',
-  300: '#CDD2D7',
-  400: '#B2BAC2',
-  500: '#A0AAB4',
-  600: '#6F7E8C',
-  700: '#3E5060',
-  800: '#2D3843',
-  900: '#1A2027',
-};
-
-const StyledInputElement = styled('input')(
-  ({ theme }) => `
-  width: 320px;
-  font-family: IBM Plex Sans, sans-serif;
-  font-size: 0.875rem;
-  font-weight: 400;
-  line-height: 1.5;
-  padding: 12px;
-  border-radius: 12px;
-  color: ${theme.palette.mode === 'dark' ? grey[300] : grey[900]};
-  background: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-  border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-  box-shadow: 0px 2px 2px ${theme.palette.mode === 'dark' ? grey[900] : grey[50]};
-
-  &:hover {
-    border-color: ${blue[400]};
-  }
-
-  &:focus {
-    border-color: ${blue[400]};
-    box-shadow: 0 0 0 3px ${theme.palette.mode === 'dark' ? blue[500] : blue[200]};
-  }
-
-  // firefox
-  &:focus-visible {
-    outline: 0;
-  }
-`,
-);
