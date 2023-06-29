541d302cbf05154097369e3dcf214ac4824e7c29
nobdey92 <jordan_hobdey@hotmail.co.uk>
2023-06-29 18:58:50 +0100

    start of search form

--- src/pages/Explore.js
+++ src/pages/Explore.js
@@ -1,6 +1,5 @@
 import Stack from "@mui/material/Stack";
 
-import { Footer } from "../components/Footer";
 import { SearchForm } from "../components/SearchForm";
 import { RecipesContainer } from "../components/RecipesContainer";
 
