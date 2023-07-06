f678e8b2bdaa2a7ef0de5f4fdfd87ebf273b24e3
GreenCatInForest <event2016.2017@gmail.com>
2023-07-06 18:06:45 +0100

    clean search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,18 +1,12 @@
 import * as React from "react";
-import InputLabel from "@mui/material/InputLabel";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import { Grid, Stack } from "@mui/material";
+
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
 import InputAdornment from "@mui/material/InputAdornment";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
-import FormGroup from "@mui/material/FormGroup";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Button from "@mui/material/Button";
 
 export const SearchForm = ({ setSearchQuery }) => {
@@ -50,38 +44,6 @@ export const SearchForm = ({ setSearchQuery }) => {
           ),
         }}
       />
-      {/* <FormControl fullWidth>
-        <InputLabel id="demo-simple-select-label">Select diet</InputLabel>
-        <Select
-          labelId="demo-simple-select-label"
-          id="demo-simple-select"
-          //value={age}
-          label="Select diet"
-          onChange={formik.handleChange}
-        >
-          <MenuItem value="Gluten Free">Gluten Free</MenuItem>
-          <MenuItem value="Vegetarian">Vegetarian</MenuItem>
-          <MenuItem value="Ketogenic">Ketogenic</MenuItem>
-          <MenuItem value="Vegan">Vegan</MenuItem>
-        </Select>
-      </FormControl>
-
-      <FormGroup>
-        <Grid container justifyContent="center" alignItems="center" columns={3}>
-          <FormControlLabel control={<Checkbox />} label="Dairy" />
-          <FormControlLabel control={<Checkbox />} label="Egg" />
-          <FormControlLabel control={<Checkbox />} label="Gluten" />
-          <FormControlLabel control={<Checkbox />} label="Grain" />
-          <FormControlLabel control={<Checkbox />} label="Peanut" />
-          <FormControlLabel control={<Checkbox />} label="Seafood" />
-          <FormControlLabel control={<Checkbox />} label="Sesame" />
-          <FormControlLabel control={<Checkbox />} label="Shellfish" />
-          <FormControlLabel control={<Checkbox />} label="Soy" />
-          <FormControlLabel control={<Checkbox />} label="Sulfite" />
-          <FormControlLabel control={<Checkbox />} label="Tree Nut" />
-          <FormControlLabel control={<Checkbox />} label="Wheat" />
-        </Grid>
-      </FormGroup> */}
       <Button variant="contained" type="submit">
         Explore
       </Button>
