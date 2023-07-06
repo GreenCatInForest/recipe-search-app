ca420015bf6264868a1c57e2439eea22d8b9c89e
GreenCatInForest <event2016.2017@gmail.com>
2023-07-06 21:13:28 +0100

    styled features component

--- src/components/Features.js
+++ src/components/Features.js
@@ -1,4 +1,103 @@
-export const Features = () => {
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import Stack from "@mui/material/Stack";
+import { createTheme, ThemeProvider } from "@mui/material";
+import aDish from "../img/aDish.png";
+import severalDishes from "../img/severalDishes.png";
+import menu from "../img/menu.png";
 
-return (   <div>Features</div> )
-}
+export const Features = () => {
+  const theme = createTheme({
+    typography: {
+      fontFamily: ["Pacifico"].join(","),
+    },
+  });
+  return (
+    <Stack>
+      <ThemeProvider theme={theme}>
+        <Typography
+          gutterBottom
+          variant="h3"
+          component="div"
+          textAlign="center"
+        >
+          Features
+        </Typography>
+      </ThemeProvider>
+      <Stack
+        flexDirection={{ xs: "column", sm: "column", md: "row" }}
+        alignItems="center"
+        justifyContent="space-between"
+        spacing={2}
+        sx={{ p: 2 }}
+      >
+        <Card sx={{ maxWidth: 345, justifyContent: "center" }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height={300}
+              image={aDish}
+              alt="a fancy dish"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h6"
+                component="div"
+                textAlign="center"
+              >
+                More than 1000 recipes
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height={280}
+              image={severalDishes}
+              alt="a fancy dishes"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h6"
+                component="div"
+                textAlign="center"
+              >
+                Create your own diet plan
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+
+        <Card sx={{ maxWidth: 345 }}>
+          <CardActionArea>
+            <CardMedia
+              component="img"
+              height={280}
+              image={menu}
+              alt="a menu picture"
+            />
+            <CardContent>
+              <Typography
+                gutterBottom
+                variant="h6"
+                component="div"
+                textAlign="center"
+              >
+                Keep on track calories & nutrition
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Stack>
+    </Stack>
+  );
+};
